44a59996207b99698f87de7995f31c6bc4fc48fc
Aimable <niyoai@yahoo.fr>
2018-06-14 13:39:22 +0100

    express - body-parser

---
+++ playground/httpster.js
@@ -0,0 +1,2 @@
+//  httpster -p 3000 -d ./public/  => this command runs all the files are in public folder on port 3000
+// ./node_modules/httpster/bin/httpster -p 3000 -d ./public/ => when is installed locally
